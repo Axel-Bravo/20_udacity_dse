103d3904f7fbfa7dbafba63300a3b710ecf394ce
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2020-04-12 14:28:52 +0200

    feat: create_tables logic properly implemented

--- 002 - Cloud Data Warehouses/004 - Project 3 - Data Warehouse/create_tables.py
+++ 002 - Cloud Data Warehouses/004 - Project 3 - Data Warehouse/create_tables.py
@@ -1,31 +1,57 @@
 import configparser
 import psycopg2
-from sql_queries import create_table_queries, drop_table_queries
+
+from .sql_queries import create_table_queries, drop_table_queries
 
 
-def drop_tables(cur, conn):
-    for query in drop_table_queries:
-        cur.execute(query)
-        conn.commit()
+def drop_tables(*, connector: psycopg2.connect, cursor, drop_queries: [str, ...]) -> None:
+    """
+    Drops tables on the DB Schema
+
+    Args:
+        connector: database connector
+        cursor: database cursor
+        drop_queries: drop tables SQL queries list
+
+    Returns: None
+    """
+    for query in drop_queries:
+        cursor.execute(query)
+        connector.commit()
 
 
-def create_tables(cur, conn):
-    for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
+def create_tables(*, connector: psycopg2.connect, cursor, create_queries: [str, ...]) -> None:
+    """
+    Creates tables on the DB schema
+
+    Args:
+        connector: database connector
+        cursor: database cursor
+        create_queries: create tables SQL queries list
+
+    Returns: None
+    """
+    for query in create_queries:
+        cursor.execute(query)
+        connector.commit()
 
 
 def main():
+    """
+    Create tables main method; resets tables to zero for a given data base
+    Returns: None
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
+    connector = psycopg2.connect("host={} dbname={} user={} password={} port={}"
+                                 .format(*config['CLUSTER'].values()))
+    cursor = connector.cursor()
 
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
+    drop_tables(connector=connector, cursor=cursor, drop_queries=drop_table_queries)
+    create_tables(connector=connector, cursor=cursor, create_queries=create_table_queries)
 
-    conn.close()
+    connector.close()
 
 
 if __name__ == "__main__":
