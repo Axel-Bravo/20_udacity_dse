0534362b87611893ffb400a03469c8504ddd573d
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2020-04-12 19:59:48 +0200

    code: etl logic properly refactored to conform python standards

--- 002 - Cloud Data Warehouses/004 - Project 3 - Data Warehouse/etl.py
+++ 002 - Cloud Data Warehouses/004 - Project 3 - Data Warehouse/etl.py
@@ -1,31 +1,59 @@
 import configparser
 import psycopg2
+
 from sql_queries import copy_table_queries, insert_table_queries
 
 
-def load_staging_tables(cur, conn):
-    for query in copy_table_queries:
-        cur.execute(query)
-        conn.commit()
+def load_staging_tables(*, connector: psycopg2.connect, cursor, copy_queries: [str, ...]) -> None:
+    """
+    Drops tables on the DB Schema
+
+    Args:
+        connector: database connector
+        cursor: database cursor
+        copy_queries: copy information form the S3 bucket into DB table SQL queries list
+
+    Returns: None
+    """
+
+    for query in copy_queries:
+        cursor.execute(query)
+        connector.commit()
 
 
-def insert_tables(cur, conn):
-    for query in insert_table_queries:
-        cur.execute(query)
-        conn.commit()
+def insert_tables(*, connector: psycopg2.connect, cursor, insert_queries: [str, ...]) -> None:
+    """
+    Drops tables on the DB Schema
+
+    Args:
+        connector: database connector
+        cursor: database cursor
+        insert_queries: insert information form the landing into staging DB tables's queries list
+
+    Returns: None
+    """
+
+    for query in insert_queries:
+        cursor.execute(query)
+        connector.commit()
 
 
 def main():
+    """
+    Load tables main method; introduces data to the appropiate tables on the database
+    Returns: None
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
-    
-    load_staging_tables(cur, conn)
-    insert_tables(cur, conn)
+    connector = psycopg2.connect("host={} dbname={} user={} password={} port={}"
+                                 .format(*config['CLUSTER'].values()))
+    cursor = connector.cursor()
 
-    conn.close()
+    load_staging_tables(connector=connector, cursor=cursor, copy_queries=copy_table_queries)
+    insert_tables(connector=connector, cursor=cursor, insert_queries=insert_table_queries)
+
+    connector.close()
 
 
 if __name__ == "__main__":
