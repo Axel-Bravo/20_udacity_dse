14a9d701bf2a6f618875bc7cb961761f864362b5
Axel Bravo Mutllo <axel.bravo.mutllo@protonmail.com>
2020-05-14 21:42:46 +0200

    chore: correct proper answer and exercices files

--- 004 - Data Pipelines with Airflow/003 - Lesson 3 - Production Data Pipelines/019 - Exercice 4/facts_calculator_official_solution.py
+++ 004 - Data Pipelines with Airflow/003 - Lesson 3 - Production Data Pipelines/019 - Exercice 4/facts_calculator_official_solution.py
@@ -28,17 +28,18 @@ class FactsCalculatorOperator(BaseOperator):
                  *args, **kwargs):
 
         super(FactsCalculatorOperator, self).__init__(*args, **kwargs)
-        #
-        # TODO: Set attributes from __init__ instantiation arguments
-        #
+        self.redshift_conn_id = redshift_conn_id
+        self.origin_table = origin_table
+        self.destination_table = destination_table
+        self.fact_column = fact_column
+        self.groupby_column = groupby_column
 
     def execute(self, context):
-        #
-        # TODO: Fetch the redshift hook
-        #
-
-        #
-        # TODO: Format the `facts_sql_template` and run the query against redshift
-        #
-
-        pass
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        facts_sql = FactsCalculatorOperator.facts_sql_template.format(
+            origin_table=self.origin_table,
+            destination_table=self.destination_table,
+            fact_column=self.fact_column,
+            groupby_column=self.groupby_column
+        )
+        redshift.run(facts_sql)
