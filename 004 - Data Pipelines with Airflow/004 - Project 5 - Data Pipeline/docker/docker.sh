64ef17a6ec3bd41bec8ba81063902a28aee8e99b
Axel Bravo Mutllo <axel.bravo.mutllo@protonmail.com>
2020-05-14 21:00:23 +0200

    chore: update docker related files

--- 004 - Data Pipelines with Airflow/004 - Project 5 - Data Pipeline/docker/docker.sh
+++ 004 - Data Pipelines with Airflow/004 - Project 5 - Data Pipeline/docker/docker.sh
@@ -1,1 +1,2 @@
-docker run -d -p 8080:8080 -v "$(pwd)"/dags:/usr/local/airflow/dags -v "$(pwd)"/requirements.txt:/requirements.txt -v "$(pwd)"/plugins:/usr/local/airflow/plugins --name airflow_solo puckel/docker-airflow webserver
+docker run -d -p 8080:8080 -v "$(pwd)"/dags:/usr/local/airflow/dags -v "$(pwd)"/requirements.txt:/requirements.txt \
+-v "$(pwd)"/plugins:/usr/local/airflow/plugins --name airflow_solo puckel/docker-airflow webserver
