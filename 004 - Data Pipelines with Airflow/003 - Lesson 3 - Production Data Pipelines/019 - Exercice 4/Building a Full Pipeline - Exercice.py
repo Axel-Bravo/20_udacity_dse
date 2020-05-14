65045e409e499a1f68530b5fe03743adbb04e3c5
Axel Bravo Mutllo <axel.bravo.mutllo@protonmail.com>
2020-05-14 21:36:37 +0200

    feat: exercice 4 and its official solution uploaded

--- 004 - Data Pipelines with Airflow/003 - Lesson 3 - Production Data Pipelines/019 - Exercice 4/Building a Full Pipeline - Exercice.py
+++ 004 - Data Pipelines with Airflow/003 - Lesson 3 - Production Data Pipelines/019 - Exercice 4/Building a Full Pipeline - Exercice.py
@@ -9,7 +9,7 @@ from airflow.operators import (
 )
 
 #
-# The following DAG performs the following functions:
+# TODO: Create a DAG which performs the following functions:
 #
 #       1. Loads Trip data from S3 to RedShift
 #       2. Performs a data quality check on the Trips table in RedShift
@@ -20,46 +20,23 @@ from airflow.operators import (
 dag = DAG("lesson3.exercise4", start_date=datetime.datetime.utcnow())
 
 #
-# The following code will load trips data from S3 to RedShift. Use the s3_key
+# TODO: Load trips data from S3 to RedShift. Use the s3_key
 #       "data-pipelines/divvy/unpartitioned/divvy_trips_2018.csv"
 #       and the s3_bucket "udacity-dend"
 #
-copy_trips_task = S3ToRedshiftOperator(
-    task_id="load_trips_from_s3_to_redshift",
-    dag=dag,
-    table="trips",
-    redshift_conn_id="redshift",
-    aws_credentials_id="aws_credentials",
-    s3_bucket="udacity-dend",
-    s3_key="data-pipelines/divvy/unpartitioned/divvy_trips_2018.csv"
-)
+#copy_trips_task = S3ToRedshiftOperator(...)
 
 #
-#  Data quality check on the Trips table
+# TODO: Perform a data quality check on the Trips table
 #
-check_trips = HasRowsOperator(
-    task_id="check_trips_data",
-    dag=dag,
-    redshift_conn_id="redshift",
-    table="trips"
-)
+#check_trips = HasRowsOperator(...)
 
 #
-# We use the FactsCalculatorOperator to create a Facts table in RedShift. The fact column is
-#  `tripduration` and the groupby_column is `bikeid`
+# TODO: Use the FactsCalculatorOperator to create a Facts table in RedShift. The fact column should
+#       be `tripduration` and the groupby_column should be `bikeid`
 #
-calculate_facts = FactsCalculatorOperator(
-    task_id="calculate_facts_trips",
-    dag=dag,
-    redshift_conn_id="redshift",
-    origin_table="trips",
-    destination_table="trips_facts",
-    fact_column="tripduration",
-    groupby_column="bikeid"
-)
+#calculate_facts = FactsCalculatorOperator(...)
 
 #
-# Task ordering for the DAG tasks 
+# TODO: Define task ordering for the DAG tasks you defined
 #
-copy_trips_task >> check_trips
-check_trips >> calculate_facts
