aff200ca6c4a5cd5ef1220246e989bd65ecca4f9
Axel Bravo Mutllo <axel.bravo.mutllo@protonmail.com>
2020-05-16 12:36:22 +0200

    feat: Project Data Pipeline - Step: Configuring the DAG

---
+++ 004 - Data Pipelines with Airflow/004 - Project 5 - Data Pipeline/udac_example_dag.py
@@ -0,0 +1,78 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators import (
+    DataQualityOperator, LoadDimensionOperator, LoadFactOperator, StageToRedshiftOperator,
+    )
+from airflow.operators.dummy_operator import DummyOperator
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+        'owner': 'udacity',
+        'start_date': datetime.datetime(2019, 1, 12),
+        'depends_on_past': False,
+        'retries': 3,
+        'retry_delay': datetime.timedelta(minutes=5),
+        'catchup': False,
+        'email_on_retry': False
+        }
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *'
+          )
+
+start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_events',
+        dag=dag
+        )
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_songs',
+        dag=dag
+        )
+
+load_songplays_table = LoadFactOperator(
+        task_id='Load_songplays_fact_table',
+        dag=dag
+        )
+
+load_user_dimension_table = LoadDimensionOperator(
+        task_id='Load_user_dim_table',
+        dag=dag
+        )
+
+load_song_dimension_table = LoadDimensionOperator(
+        task_id='Load_song_dim_table',
+        dag=dag
+        )
+
+load_artist_dimension_table = LoadDimensionOperator(
+        task_id='Load_artist_dim_table',
+        dag=dag
+        )
+
+load_time_dimension_table = LoadDimensionOperator(
+        task_id='Load_time_dim_table',
+        dag=dag
+        )
+
+run_quality_checks = DataQualityOperator(
+        task_id='Run_data_quality_checks',
+        dag=dag
+        )
+
+end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
+
+# DAG
+start_operator >> [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+load_songplays_table >> [load_user_dimension_table, load_song_dimension_table]
+load_songplays_table >> [load_artist_dimension_table, load_time_dimension_table]
+[load_user_dimension_table, load_song_dimension_table] >> run_quality_checks
+load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
+run_quality_checks >> end_operator
