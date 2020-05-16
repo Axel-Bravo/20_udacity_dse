b6c5e78164b7ed692b88c5c22db573719a2086f5
Axel Bravo Mutllo <axel.bravo.mutllo@protonmail.com>
2020-05-16 13:24:37 +0200

    feat: Project Data Pipeline - Step: Building the operators - Fact and Dimension Operators

---
+++ 004 - Data Pipelines with Airflow/004 - Project 5 - Data Pipeline/sql_queries.py
@@ -0,0 +1,69 @@
+class SqlQueries:
+    songplay_table_insert = ("""
+        DROP TABLE IF EXISTS songplay_table;
+        CREATE TABLE songplay_table AS
+            SELECT
+                    md5(events.sessionid || events.start_time) songplay_id,
+                    events.start_time, 
+                    events.userid, 
+                    events.level, 
+                    songs.song_id, 
+                    songs.artist_id, 
+                    events.sessionid, 
+                    events.location, 
+                    events.useragent
+                    FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
+                FROM staging_events
+                WHERE page='NextSong') events
+                LEFT JOIN staging_songs songs
+                ON events.song = songs.title
+                    AND events.artist = songs.artist_name
+                    AND events.length = songs.duration;
+    """)
+
+    user_table_insert = ("""
+        DROP TABLE IF EXISTS user_table;
+        CREATE TABLE user_table AS
+            SELECT distinct userid,
+                   firstname,
+                   lastname,
+                   gender,
+                   level
+            FROM staging_events
+            WHERE page='NextSong';
+    """)
+
+    song_table_insert = ("""
+        DROP TABLE IF EXISTS song_table;
+        CREATE TABLE song_table AS
+            SELECT distinct song_id,
+                   title,
+                   artist_id, 
+                   year, 
+                   duration
+            FROM staging_songs;
+    """)
+
+    artist_table_insert = ("""
+        DROP TABLE IF EXISTS artist_table;
+        CREATE TABLE artist_table AS
+            SELECT distinct artist_id, 
+                   artist_name, 
+                   artist_location, 
+                   artist_latitude, 
+                   artist_longitude
+            FROM staging_songs;
+    """)
+
+    time_table_insert = ("""
+        DROP TABLE IF EXISTS time_table;
+        CREATE TABLE time_table AS
+            SELECT start_time, 
+                   extract(hour from start_time), 
+                   extract(day from start_time), 
+                   extract(week from start_time), 
+                   extract(month from start_time),
+                   extract(year from start_time), 
+                   extract(dayofweek from start_time)
+            FROM songplays;
+    """)
--- 004 - Data Pipelines with Airflow/004 - Project 5 - Data Pipeline/udac_example_dag.py
+++ 004 - Data Pipelines with Airflow/004 - Project 5 - Data Pipeline/udac_example_dag.py
@@ -2,12 +2,11 @@ import datetime
 
 from airflow import DAG
 from airflow.operators import (
-    DataQualityOperator, LoadDimensionOperator, LoadFactOperator, StageToRedshiftOperator,
+    DataQualityOperator, StageToRedshiftOperator,
     )
 from airflow.operators.dummy_operator import DummyOperator
-
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
+from airflow.operators.postgres_operator import PostgresOperator
+from helpers import SqlQueries
 
 default_args = {
         'owner': 'udacity',
@@ -37,29 +36,44 @@ stage_songs_to_redshift = StageToRedshiftOperator(
         dag=dag
         )
 
-load_songplays_table = LoadFactOperator(
+load_songplays_table = PostgresOperator(
         task_id='Load_songplays_fact_table',
-        dag=dag
+        dag=dag,
+        sql=SqlQueries.songplay_table_insert,
+        autocommit=True,
+        postgres_conn_id="redshift"
         )
 
-load_user_dimension_table = LoadDimensionOperator(
+load_user_dimension_table = PostgresOperator(
         task_id='Load_user_dim_table',
-        dag=dag
+        dag=dag,
+        sql=SqlQueries.user_table_insert,
+        autocommit=True,
+        postgres_conn_id="redshift"
         )
 
-load_song_dimension_table = LoadDimensionOperator(
+load_song_dimension_table = PostgresOperator(
         task_id='Load_song_dim_table',
-        dag=dag
+        dag=dag,
+        sql=SqlQueries.song_table_insert,
+        autocommit=True,
+        postgres_conn_id="redshift"
         )
 
-load_artist_dimension_table = LoadDimensionOperator(
+load_artist_dimension_table = PostgresOperator(
         task_id='Load_artist_dim_table',
-        dag=dag
+        dag=dag,
+        sql=SqlQueries.artist_table_insert,
+        autocommit=True,
+        postgres_conn_id="redshift"
         )
 
-load_time_dimension_table = LoadDimensionOperator(
+load_time_dimension_table = PostgresOperator(
         task_id='Load_time_dim_table',
-        dag=dag
+        dag=dag,
+        sql=SqlQueries.time_table_insert,
+        autocommit=True,
+        postgres_conn_id="redshift"
         )
 
 run_quality_checks = DataQualityOperator(
@@ -74,5 +88,5 @@ start_operator >> [stage_events_to_redshift, stage_songs_to_redshift] >> load_so
 load_songplays_table >> [load_user_dimension_table, load_song_dimension_table]
 load_songplays_table >> [load_artist_dimension_table, load_time_dimension_table]
 [load_user_dimension_table, load_song_dimension_table] >> run_quality_checks
-load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
+[load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
 run_quality_checks >> end_operator
